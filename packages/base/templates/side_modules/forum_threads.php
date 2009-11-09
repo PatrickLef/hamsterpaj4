5c4f81fc862b712a75d15b4c6636dd25e63ffd96
Patrick Lef <lef@hamsterpaj.net>
2009-11-09 15:18:33 +0100

    Fixed timestamp in forum threads module
    Signed-off-by: Patrick Lef <lef@hamsterpaj.net>

--- packages/base/templates/side_modules/forum_threads.php
+++ packages/base/templates/side_modules/forum_threads.php
@@ -5,7 +5,7 @@
 		<li class="<?php echo $counter; ?>">
 			<?php	$thread['title_short'] = (mb_strlen($thread['title'], 'UTF8') > 22) ? mb_substr($thread['title'], 0, 22, 'UTF8') . '...' : $thread['title']; ?>
 			<?php $info = 'I ' . $thread['category_title'] . ' av ' . $thread['username'] . ': ' . $thread['title']; ?>
-			<span class="ui_module_latest_posts_written"><?php echo date('H:i', $thread['last_post_timestamp']); ?></span>
+			<span class="ui_module_latest_posts_written"><?php echo date('H:i', $thread['timestamp']); ?></span>
 			<a title="<?php echo $info; ?>" href="<?php echo $thread['url']; ?>"><?php echo $thread['title_short']; ?></a>
 		</li>
 	<?php endforeach; ?>
